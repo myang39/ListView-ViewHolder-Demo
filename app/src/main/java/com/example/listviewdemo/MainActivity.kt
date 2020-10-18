3fec8bb663b6f7d6eb9e4c13dfd6ad2375bc344f
Luke Yang <luke.yang@verizonmedia.com>
2020-10-17 21:52:56 -0700

    using viewHolder and kotlin-android-extensions

--- app/src/main/java/com/example/listviewdemo/MainActivity.kt
+++ app/src/main/java/com/example/listviewdemo/MainActivity.kt
@@ -11,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ListView
 import android.widget.TextView
+import kotlinx.android.synthetic.main.row_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,20 +48,24 @@ class MainActivity : AppCompatActivity() {
             if (convertView == null) {
                 val layoutInflater = LayoutInflater.from(myContext)
                 rowMain = layoutInflater.inflate(R.layout.row_main, parent, false)
+
+                Log.d("Main", "calling findViewById, which is expensive")
+                val viewHolder = ViewHolder(rowMain.name_textview, rowMain.position_textview)
+                rowMain.tag = viewHolder
             } else {
                 rowMain = convertView
             }
 
-            Log.d("Main", "calling findViewById, which is expensive")
-            val nameTextView = rowMain.findViewById<TextView>(R.id.name_textview)
-            nameTextView.text = names[position]
+            val viewHolder = rowMain.tag as ViewHolder
 
-            Log.d("Main", "calling findViewById, which is expensive")
-            val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
-            positionTextView.text = "Row Number: $position"
+            viewHolder.nameTextView.text = names[position]
+
+            viewHolder.positionTextView.text = "Row Number: $position"
 
             return rowMain
         }
+
+        private class ViewHolder(val nameTextView: TextView, val positionTextView: TextView)
 
     }
 }
